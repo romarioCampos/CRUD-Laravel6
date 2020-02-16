26b650b6d2d628b45b19c4b1d5c30df5b47c3f06
Romário Campos <oliveira-campos@outlook.com.br>
2020-02-16 19:45:37 -0300

    updated

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,19 @@
 // ShowController
 
 Route::get('/', function () {
+    return view('create');
+});
+
+Auth::routes();
+
+Route::get('/shows', 'ShowController@create')->name('create');
+
+Route::resource('shows', 'ShowController');
+
+
+/*
+Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('shows', 'ShowController');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
